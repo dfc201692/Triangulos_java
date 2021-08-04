d7976d30bac182c698fa873e5e347f4f6043edb8
david.cruz <dfcc15@hotmail.com>
2021-08-04 13:31:25 -0500

    Triangulo

---
+++ src/triangulo/Triangulo.java
@@ -0,0 +1,14 @@
+package triangulo;
+
+/*AUTOR: David Cruz*/
+
+public class Triangulo {
+
+    public static void main(String[] args) {
+
+        Proyecto_Triangulos f = new Proyecto_Triangulos();
+        f.setVisible(true);
+
+    }
+
+}
